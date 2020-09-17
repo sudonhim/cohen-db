42017ea07c5e667ad97b55360f657cfb471c95f3
Brenton Milne <brmilne@microsoft.com>
2020-09-16 23:59:53 -0700

    further work on generate-syms

--- run/generate-syms.ts
+++ run/generate-syms.ts
@@ -5,22 +5,25 @@ interface SymbolEntry {
     // How to display the word in UI
     title: string;
 
-    // The stemmed version of the word and synonyms,
-    // e.g. "darkness" -> "dark", "dim"
-    stemmedVariants: string[];
+    // ID and filename, e.g. "symbol.darkness"
+    id: string;
 
     // Conceptually related, e.g. dark => night
     // these are in id form, and must exist in the SymbolsMap
     related: string[];
+
+    // The stemmed version of the word and synonyms,
+    // e.g. "darkness" -> "dark", "dim"
+    stems: string[];
 }
 
 interface SymbolsMap {
     [id: string]: SymbolEntry
 }
 
-const symsFile = fs.readFileSync('./syms/wordgroups.txt', 'utf8');
-const wordGroups = symsFile.split(/[\r\n]+/).map(grp =>
-    grp.split(', '));
+// Build map of word => [related words]
+const wordGroups = fs.readFileSync('./syms/wordgroups.txt', 'utf8')
+    .split(/[\r\n]+/).map(grp => grp.split(', '));
 const related: {[key: string]: string[]} = {};
 for (var grp of wordGroups) {
     for (var word of grp) {
@@ -33,14 +36,27 @@ for (var grp of wordGroups) {
     }
 }
 
+// Load synonyms
+const synsEntries = fs.readFileSync('./syms/synonyms.txt', 'utf8')
+    .split(/[\r\n]+/).map(grp => grp.split(', '));
+const synsLookup: {[key: string]: string[]} = {};
+synsEntries.forEach(entry => {
+    const [word, ...synonyms] = entry;
+    synsLookup[word] = synonyms;
+});
+
+// Generate SymbolsMap to be used for search
 const symsMap: SymbolsMap = {};
 for (var word in related) {
     const key = stemmer(word);
     symsMap[key] = {
         title: word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase(),
-        related: related[word].map(w => stemmer(w)),
-        stemmedVariants: [stemmer(word)]
+        id: `symbol.${word.replace(' ', '_')}`,
+        related: related[word],
+        stems: [stemmer(word), ...(synsLookup[word] || []).map(syn => stemmer(syn))]
     }
 }
+
+
 
 
