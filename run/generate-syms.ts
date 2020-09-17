3bef47f80dffb4239218af633e858dfe599c1509
Brenton Milne <brenton.a.milne@gmail.com>
2020-09-16 21:35:27 -0700

    update syms files

--- run/generate-syms.ts
+++ run/generate-syms.ts
@@ -1,7 +1,22 @@
 import * as fs from 'fs';
 import * as stemmer from 'stemmer';
 
-import { SymbolsMap } from '../syms';
+interface SymbolEntry {
+    // How to display the word in UI
+    title: string;
+
+    // The stemmed version of the word and synonyms,
+    // e.g. "darkness" -> "dark", "dim"
+    stemmedVariants: string[];
+
+    // Conceptually related, e.g. dark => night
+    // these are in id form, and must exist in the SymbolsMap
+    related: string[];
+}
+
+interface SymbolsMap {
+    [id: string]: SymbolEntry
+}
 
 const symsFile = fs.readFileSync('./syms/wordgroups.txt', 'utf8');
 const wordGroups = symsFile.split(/[\r\n]+/).map(grp =>
