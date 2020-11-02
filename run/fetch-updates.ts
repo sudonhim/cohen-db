dd86e057623e3f1166ea4dbe52a18f45a53dffd0
Brenton Milne <brenton.a.milne@gmail.com>
2020-11-01 20:31:52 -0800

    fix name for symbols committed by symbols bot

--- run/fetch-updates.ts
+++ run/fetch-updates.ts
@@ -1,27 +1,17 @@
-import axios from 'axios';
 import { LoadAndValidate, ValidateAndSave } from '../lib/utils';
 import { CanonDb } from '../index';
-import { Annotation } from '../schema';
-
-interface UpdatesBlob {
-    [docId: string]: Annotation[]
-}
 
 const docDb: CanonDb = LoadAndValidate();
 console.log(`Loaded and validated ${Object.keys(docDb).length} documents`);
 
 async function run() {
-    const src = 'https://cohen-machine.herokuapp.com/api/updates';
-    console.log(`Fetching updates from [${src}]...`);
-    const data = await (await axios.get(src)).data as UpdatesBlob;
-    console.log('Response received:');
-    console.log(JSON.stringify(data, null, 2));
-
-    for (var docId in data) {
-        const newAnnotations: Annotation[] = data[docId] as Annotation[];
-        const allAnnotations = [...(docDb[docId].annotations || []), ...newAnnotations];
-        allAnnotations.sort((a, b) => a.anchor.localeCompare(b.anchor));
-        docDb[docId].annotations = allAnnotations;
+    for (var docId in docDb) {
+        const doc = docDb[docId];
+        const curAnnos = (doc.annotations || []);
+        curAnnos.forEach(anno => {
+            if (anno.tokens.length === 1 && anno.tokens[0].kind === 'docref' && anno.tokens[0].docRef.startsWith('sym'))
+                anno.user = 'symbols bot'
+        });
     }
 
     console.log(`Begin validate and save...`);
