985ae405ce66d75fb0e75e479c20c903873bc3a5
Brenton Milne <brenton.a.milne@gmail.com>
2020-12-21 15:48:28 -0600

    fix section id references

--- run/generate-new-refs.ts
+++ run/generate-new-refs.ts
@@ -1,43 +1,26 @@
-import { LoadAndValidate, ValidateAndSave } from '../lib/utils';
-import { CanonDb } from '../index';
-import { Reference } from '../schema';
+import { LoadAndValidate, ValidateAndSave } from "../lib/utils";
+import { CanonDb } from "../index";
+import { Reference } from "../schema";
 
 const docDb: CanonDb = LoadAndValidate();
 
 console.log(`Loaded and validated ${Object.keys(docDb).length} documents`);
 
 for (const id in docDb) {
-    const doc = docDb[id];
-    if (!doc.content) continue;
-    for (const grp of doc.annotations) { 
-        let ref: Reference;
-        if (doc.content.kind === 'multipart') {
-            const [sectionId, fragmentId] = grp.newAnchor.split(':');
-            if (fragmentId) {
-                ref = {
-                    kind: 'fragment',
-                    documentId: id,
-                    sectionId,
-                    fragmentId
-                };
-            } else {
-                ref = {
-                    kind: 'section',
-                    documentId: id,
-                    sectionId
-                };
-            }
-        } else {
-            ref = {
-                kind: 'fragment',
-                documentId: id,
-                fragmentId: grp.newAnchor
-            };
+  const doc = docDb[id];
+  if (!doc.content) continue;
+  if (doc.content.kind === "multipart") {
+    for (const grp of doc.annotations) {
+        if (grp.anchor.kind === 'section' || grp.anchor.kind === 'fragment') {
+            if (!grp.anchor.sectionId) throw 'Expected section id for ' + id;
+            const si = parseInt(grp.anchor.sectionId);
+            grp.anchor.sectionId = doc.content.content[si - 1].id;
         }
-        grp.anchor = ref;
-        delete grp.newAnchor;
     }
+  }
 }
 
 ValidateAndSave(docDb);
-console.log(`Validated and saved. If there are any changes, roundtrip is broken.`);
+console.log(
+  `Validated and saved. If there are any changes, roundtrip is broken.`
+);
