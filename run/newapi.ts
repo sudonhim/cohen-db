ec99cec53995f5be94415654cca379f047009dfd
Brenton Milne <brenton.a.milne@gmail.com>
2021-01-09 10:43:51 -0600

    strip out 'speaker' prop

--- run/newapi.ts
+++ run/newapi.ts
@@ -7,7 +7,40 @@ const docDb: CanonDb = LoadAndValidate();
 
 console.log(`Loaded and validated ${Object.keys(docDb).length} documents`);
 
-const newContent = [
+// remove the 'speaker' property of text fragments
+// instead just use a fragment with inactive text
+for (const documentId in docDb) {
+  const doc = docDb[documentId];
+  if (doc.content && doc.content.kind === 'simple') {
+    const newFragments: Fragment[] = [];
+    const prevFrag: Fragment = null;
+    for (const frag of doc.content.content.fragments) {
+      if (frag.kind === 'text') {
+        if (frag.speaker && (!prevFrag || prevFrag.kind !== 'text' || prevFrag.speaker !== frag.speaker)) {
+          newFragments.push({
+            kind: 'text',
+            tokens: [
+              { kind: 'text', secondary: true, text: frag.speaker + ':' }
+            ]
+          })
+        }
+
+        const modFrag: Fragment = {
+          kind: 'text',
+          id: frag.id,
+          tokens: frag.tokens
+        };
+        newFragments.push(modFrag);
+
+      } else { newFragments.push(frag); }
+    }
+
+    doc.content.content.fragments = newFragments;
+  }
+}
+
+// old code for text format
+/* const newContent = [
   './data/1984ckuainterview.txt',
   //'./data/2016youwantitdarker.txt',
   //'./data/2017lastinterview.txt'
@@ -84,7 +117,7 @@ for (const data of newContent) {
   const fullId = 'interview.' + documentId;
   docDb[fullId] = newDoc;
   // docDb['group.interviews'].children.push('interview.' + documentId);
-}
+} */
 
 ValidateAndSave(docDb);
 
